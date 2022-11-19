112f63a6e73b2b744fdd57a8697b16a00f875919
0xffff00 <juldre6@gmail.com>
2022-11-19 21:52:40 +0100

    Projektdateien hinzufügen.

---
+++ Oxfobot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Discord.WebSocket;
+using System.Net.Http.Headers;
+
+namespace Oxfobot
+{
+    internal class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            if (!File.Exists("key.txt"))
+            {
+                Console.WriteLine("Keyfile does not exists, creating one...");
+                File.WriteAllText("key.txt", "");
+                return;
+            }
+
+            _client = new DiscordSocketClient();
+            _client.Log += _client_Log;
+            await _client.LoginAsync(Discord.TokenType.Bot, File.ReadAllText("key.txt"));
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(Discord.LogMessage arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
