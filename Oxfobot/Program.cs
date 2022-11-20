e433f7944026ff0247ff56e8373ba44bc0f5df71
0xffff00 <juldre6@gmail.com>
2022-11-20 15:41:31 +0100

    Added _client_MessageReceived function

--- Oxfobot/Program.cs
+++ Oxfobot/Program.cs
@@ -20,9 +20,15 @@ namespace Oxfobot
 
             _client = new DiscordSocketClient();
             _client.Log += _client_Log;
+            _client.MessageReceived += _client_MessageReceived;
             await _client.LoginAsync(Discord.TokenType.Bot, File.ReadAllText("key.txt"));
             await _client.StartAsync();
             await Task.Delay(-1);
+        }
+
+        private async Task _client_MessageReceived(SocketMessage arg)
+        {
+            Console.WriteLine("MessageRecieved | " +(arg.Source) +" | " + (!arg.Author.IsBot ? "Member" : (arg.Author.IsWebhook?"Webhook":"Bot")) +$" | {arg.Author.Username}#{arg.Author.Discriminator}: {arg.Content}");
         }
 
         private Task _client_Log(Discord.LogMessage arg)
